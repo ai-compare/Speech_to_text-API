0036b221db566ae2e611a7c9a917899daafcdd92
Eden AI <67952486+ai-compare@users.noreply.github.com>
2021-07-09 14:49:08 +0200

    Update AI-Compare-Speechtotext-python-request.py

--- AI-Compare-Speechtotext-python-request.py
+++ AI-Compare-Speechtotext-python-request.py
@@ -6,10 +6,10 @@ import requests
 headers = {'Authorization': 'Bearer your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
 
 # Select API
-url = 'https://www.ai-compare.com/v1/pretrained/audio/speech_recognition'
+url = 'https://api.edenai.run/v1/pretrained/audio/speech_recognition'
 
 # Select providers, and audio to transcribe
-payload = {'providers':'[\'ibm\', \'cognitives_service\', \'aws\', \'google_cloud\']','text_to_find':'Bonjour, je suis Martin','language':'fr-FR'}
+payload = {'providers':'[\'ibm\', \'microsoft\', \'aws\', \'google_cloud\']','text_to_find':'Bonjour, je suis Martin','language':'fr-FR'}
 
 # Select file to test         
 files = {'files': open('Music/0wr69-jf4nc.wav','rb')}
